49526854ba0c39be8e1ebf66490dc403c0c8f97f
Guille Paz <guille87paz@gmail.com>
2015-07-26 12:32:53 -0300

    Add serviceworker files.

---
+++ sw.js
@@ -0,0 +1,62 @@
+'use strict';
+
+importScripts('/serviceworker-cache-polyfill.js');
+
+var CACHE_NAME = 'frontendrescue';
+var CACHE_VERSION = '0.0.1';
+
+var urlsToCache = [
+  '/',
+  '/src/css/styles.min.css',
+  '/src/css/data-uri.css',
+  '/src/assets/no-image.png',
+  '/favicon.ico'
+];
+
+/**
+ * Install
+ */
+this.addEventListener('install', function(eve) {
+  var urls = urlsToCache.map(function(url) {
+    return new Request(url, {credentials: 'include'});
+  });
+  eve.waitUntil(
+    caches.open(CACHE_NAME + '-v' + CACHE_VERSION)
+      .then(function(cache) {
+        return cache.addAll(urls);
+      })
+  );
+});
+
+/**
+ * Active / Remove old caches
+ */
+this.addEventListener('active', function(eve) {
+  var currentCacheName = CACHE_NAME + '-v' + CACHE_VERSION;
+  caches.keys().then(function(cacheNames) {
+    return Promise.all(
+      cacheNames.map(function(cacheName) {
+        if (cacheName.indexOf(CACHE_NAME) == -1) {
+          return;
+        }
+
+        if (cacheName != currentCacheName) {
+          return caches.delete(cacheName);
+        }
+      })
+    );
+  });
+});
+
+/**
+ * Fetch
+ */
+this.addEventListener('fetch', function(eve) {
+  var request = eve.request;
+  var url = new URL(request.url);
+  eve.respondWith(
+    caches.match(request).then(function(response) {
+      return response || fetch(request);
+    })
+  );
+});
